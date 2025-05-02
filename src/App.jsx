462dea7bcbdeb5777b3b7b4f5c7bd65a77be11d4
ashish <54009aashish@gmail.com>
2025-05-02 16:44:50 +0530

    Modify App

--- src/App.jsx
+++ src/App.jsx
@@ -27,16 +27,33 @@ function App() {
       alignItems: 'center', 
       justifyContent: 'center', 
       height: '100vh', 
-      backgroundColor: '#f0f0f0', 
-      padding: '20px', 
-      boxSizing: 'border-box' 
+      position: 'relative', 
+      overflow: 'hidden' 
     }}>
+      {/* Background Video */}
+      <video 
+        autoPlay 
+        loop 
+        muted 
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          zIndex: -1
+        }}
+      >
+        <source src="https://videos.pexels.com/video-files/3141210/3141210-uhd_2560_1440_25fps.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
       <Heading />
-      <Display  val={calval} />
+      <Display val={calval} />
       <Button clickme={onclick} />
     </div>
-  )
-  
+  )  
 }
 
 export default App
